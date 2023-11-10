9ca81b227f051642db14f58172b3211e76bd9427
Avery-Baechler <averybaechler@gmail.com>
2023-11-09 20:10:07 -0800

    working indefincation and table

--- background.js
+++ background.js
@@ -1,6 +1,32 @@
 var html;
 
+const classToToolMap = {
+  "wordpress": "WordPress",
+  "wp-": "WordPress",   
+  "wix-": "Wix",         
+  "shopify": "Shopify",
+  "squarespace": "Squarespace",
+  "weebly": "Weebly",
+  "joomla": "Joomla",
+  "webflow": "Webflow",
+  "bootstrap": "Bootstrap",
+  "elementor": "Elementor",
+  "divi-": "Divi",        
+  "godaddy": "GoDaddy",
+  "w3-": "W3Schools",  
+  "w3c": "W3C",
+  "w3schools": "W3Schools",
+  "w3school": "W3Schools", 
+  "w3css": "W3CSS",
+  "adobe-": "Adobe",  
+  "adobe": "Adobe",
+  "adobe-": "Adobe",
+  "adobe": "Adobe",
+  "react-": "React", 
+  "vue-": "Vue.js",         
+  "angular-": "Angular",    
 
+};
 
 
 
@@ -16,31 +42,38 @@ function getActiveTabDOM() {
       }); 
     }
   });
+    return html;
 }
 
-function scrapePage(data) {
+function scrapePage(data,classToToolMap) {
+const matchingTools = new Map();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const elements = doc.querySelectorAll('*');
 
-    const domData = data;
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(domData, 'text/html');
+  elements.forEach(element => {
+    const classList = Array.from(element.classList);
 
-    const className = 'parent'; 
+     for (const className of classList) {
+      for (const partialClass in classToToolMap) {
+        if (className.toLowerCase().includes(partialClass.toLowerCase())) {
+          matchingTools.set(classToToolMap[partialClass]);
+          break; // Once a partial match is found, break the loop
+        }
+      }
+    }
+  });
 
-    const elements = doc.querySelectorAll(`.${className}`);
-
-    const elementList = Array.from(elements);
-
-    console.log(elementList);
+  return Array.from(matchingTools.keys());
 }
-
 
 
 
 function handleMessage(message,sendResponse) {
-    getActiveTabDOM()
+    html = getActiveTabDOM(); 
     console.log(html);
-    scrapePage(html);
-    return Promise.resolve({data:"test"})
+    domData = scrapePage(html,classToToolMap);
+    return Promise.resolve({name:"tools", data: domData})
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
